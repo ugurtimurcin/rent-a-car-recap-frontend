975fa1d1573398a6f78d374b02d8c07b64f43751
Ugur Timurcin <utimurcin@outlook.com>
2021-03-18 13:34:35 +0300

    Edited.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { BrandComponent } from './components/brand/brand.component';
