3b159327806653e5df638bfc89533f055d7c3160
Ugur Timurcin <utimurcin@outlook.com>
2021-03-19 10:49:47 +0300

    formsmodule,filterpipe,cardetail componnt imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
-
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { CarComponent } from './components/car/car.component';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+import { CarDetailComponent } from './components/car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     BrandComponent,
-    CarComponent
+    CarComponent,
+    FilterPipePipe,
+    CarDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
