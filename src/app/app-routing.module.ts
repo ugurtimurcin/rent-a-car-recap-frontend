2df14de39c531c5fbc370c4afdf1911517301bf6
Ugur Timurcin <utimurcin@outlook.com>
2021-03-27 21:02:12 +0300

    login component imported

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
 import { CarComponent } from './components/car/car.component';
+import { LoginComponent } from './components/login/login.component';
 import { PaymentComponent } from './components/payment/payment.component';
 import { RentalComponent } from './components/rental/rental.component';
 
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'cars/brand/:id', component: CarComponent },
   { path: 'detail/:id', component: CarDetailComponent },
   { path: 'rental/:id', component: RentalComponent },
-  { path: 'payment/:rental', component: PaymentComponent }
+  { path: 'payment/:rental', component: PaymentComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
